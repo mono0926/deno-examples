6c292a5f6dec67504e15c165102cd3576cc2453a
Masayuki Ono <mono0926@gmail.com>
2021-03-30 17:25:15 +0900

    import fetch_event_adapter

--- hello.ts
+++ hello.ts
@@ -1,4 +1,8 @@
-addEventListener("fetch", (event: any) => {
+import "https://deno.land/x/fetch_event_adapter/mod.ts";
+
+if (typeof FetchEvent !== "undefined") console.log(true);
+
+addEventListener("fetch", (event) => {
   const response = new Response("Hello World!", {
     headers: { "content-type": "text/plain" },
   });
