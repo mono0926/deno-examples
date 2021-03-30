32fe26ce48143d534d7986b7e7b71a9f5d8a683c
Masayuki Ono <mono0926@gmail.com>
2021-03-30 17:15:35 +0900

    Use fetch_event_adapter

--- hello.ts
+++ hello.ts
@@ -1,4 +1,6 @@
-addEventListener("fetch", (event: any) => {
+import "https://deno.land/x/fetch_event_adapter/mod.ts";
+
+addEventListener("fetch", (event: FetchEvent) => {
   const response = new Response("Hello World!", {
     headers: { "content-type": "text/plain" },
   });
