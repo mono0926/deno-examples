c49a28333aaed0fb55e7f4a3e67062c56f0ca0dd
Masayuki Ono <mono0926@gmail.com>
2021-03-30 17:23:50 +0900

    Delete import

--- hello.ts
+++ hello.ts
@@ -1,6 +1,4 @@
-import "https://deno.land/x/fetch_event_adapter/mod.ts";
-
-addEventListener("fetch", (event) => {
+addEventListener("fetch", (event: any) => {
   const response = new Response("Hello World!", {
     headers: { "content-type": "text/plain" },
   });
