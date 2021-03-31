de330e50352c1cd68733c892b8b854af49138dcd
Masayuki Ono <mono0926@gmail.com>
2021-03-31 10:17:09 +0900

    Add janken

---
+++ janken.ts
@@ -0,0 +1,11 @@
+// `Uncaught (in promise) ReferenceError: Access to "location"`というエラーが出ている模様
+// import "https://deno.land/x/fetch_event_adapter/mod.ts";
+
+addEventListener("fetch", (event: any) => {
+  const hands = ["✊", "✌️", "✋"];
+  event.respondWith(
+    new Response(`${hands[Math.floor(Math.random() * 3)]}`, {
+      headers: { "content-type": "text/plain; charset=UTF-8" },
+    }),
+  );
+});
